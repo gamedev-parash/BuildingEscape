f94cd8ee391dc26fb6420b8e64c9152f70d31ec6
Paras Bhandari <50918320+gamedev-parash@users.noreply.github.com>
2022-07-22 21:00:43 +0545

    Creating Building Escape

---
+++ Source/Building/BuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_API ABuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
